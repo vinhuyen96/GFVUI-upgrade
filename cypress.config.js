cdf71f36d0e105a057eac100ee875dd2db8707c9
vinhuyen96 <baoanhanh961@gmail.com>
2024-09-23 16:46:17 +0700

    Init project

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://cypressdev.stratawise.com/',
+    testIsolation: true,
+    defaultCommandTimeout: 30000,
+    chromeWebSecurity: false,
+    viewportWidth: 1366, // the most suitable screen for the desktop website
+    viewportHeight: 768, // the most suitable screen for the desktop website
+    requestTimeout: 20000,
+    numTestsKeptInMemory: 1,
+    responseTimeout: 30000,
+    pageLoadTimeout: 100000,
+    experimentalRunAllSpecs: true,
+    retries: {
+      // Configure retry attempts for `cypress run`
+      runMode: 2,
+      // Configure retry attempts for `cypress open`
+      openMode: 0
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
