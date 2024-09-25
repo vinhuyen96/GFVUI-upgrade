8f7cafaddeab51daf0868b5855d9341a512d7056
vinhuyen96 <baoanhanh961@gmail.com>
2024-09-25 10:00:39 +0700

    Next step after module created successfully

--- cypress/support/pages/modules/GridPage.js
+++ cypress/support/pages/modules/GridPage.js
@@ -5,36 +5,6 @@ class GridPage extends BasePage {
     super();
   }
 
-  checkIfRequestIsUnread(line) {
-    cy.log('Check If Request Is Unread');
-    cy.get('.glyphicon.glyphicon-edit').should('be.visible');
-
-    cy.get('tr.dx-data-row')
-      .eq(line)
-      .find('td')
-      .each(($el, index) => {
-        if (index > 0) {
-          cy.wrap($el).should('have.css', 'font-weight', '900');
-        }
-      });
-  }
-
-  checkIfRequestIsRead(line) {
-    cy.log('checkIfRequestIsRead');
-    cy.get('.glyphicon.glyphicon-edit').should('be.visible');
-    cy.get('tr.dx-data-row')
-      .eq(line)
-      .then(($row) => {
-        cy.wrap($row)
-          .find('td')
-          .each(($el, index) => {
-            if (index > 0) {
-              cy.wrap($el).should('have.css', 'font-weight', '400');
-            }
-          });
-      });
-  }
-
   createNewItem() {
     cy.get('.dx-button-content .dx-button-text')
       .contains('NEW')
@@ -42,16 +12,6 @@ class GridPage extends BasePage {
       .click();
     cy.log('abc');
     cy.get('button').contains('Submit').click();
-  }
-
-  markFirstItemAsUnread() {
-    cy.get('table tr .fa-envelope-open').first().click();
-    cy.wait(2000); // Wait for animation
-  }
-
-  markFirstItemAsRead() {
-    cy.get('table tr .fa-envelope').first().click();
-    cy.wait(2000); // Wait for animation
   }
 }
 
